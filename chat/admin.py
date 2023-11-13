9294431ac9f95e6efb5f75b43d1f1010af4d9cd6
Erfanam <s.e.hashemy.1383@gmail.com>
2023-11-13 18:03:23 +0330

    Implemented Admins Site, Models and Custom User Model

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import models
 
-# Register your models here.
+
+admin.site.register(models.User)
+admin.site.register(models.Bot)
+admin.site.register(models.Conversation)
+admin.site.register(models.Message)
--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        BASIC = "BASIC"
+        PREMIUM = "PREMIUM"
+
+    role = models.CharField(max_length=10, choices=Role.choices)
+
+
+class Bot(models.Model):
+    content = models.CharField(max_length=800)
+
+
+class Message(models.Model):
+    class CommentChoices(models.TextChoices):
+        LIKE = "LIKE"
+        DISLIKE = "DISLIKE"
+
+    message = models.CharField(max_length=100)
+    comment = models.CharField(max_length=10, choices=CommentChoices.choices)
+
+
+class Conversation(models.Model):
+    messages = models.ForeignKey(
+        Message, on_delete=models.CASCADE, default=None, null=True
+    )
