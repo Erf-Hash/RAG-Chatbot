9f0182f5a0dc269e2f07ac1131c870f530627a21
Erfanam <s.e.hashemy.1383@gmail.com>
2023-11-15 16:23:44 +0330

    Implemented login and register compeletely

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.test_func),
     path('login', views.login),
-    path('register', views.register)
+    path('register', views.register),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,28 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse, Http404
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as log_in
 from django.contrib.auth.decorators import login_required
+from .models import User
 
 
 def login(request: HttpRequest):
-    if request.method == "GET":
-        return render(request, "login.html")
+    if request.GET.get('email') is None:
+        return render(request, 'login.html')
 
-    username = request.POST["username"]
-    password = request.POST["password"]
+    email = request.GET['email']
+    password = request.GET['password']
 
-    user = authenticate(request, username=username, password=password)
-    if user is None:
+    user = User.objects.get(email=email)
+    print(user)
+    if user is None or user.check_password(password):
         raise Http404()
 
-    login(request, user)
-    return redirect("/")
+    log_in(request, user)
+    return HttpResponse('login was successful')
 
 
-def register(request):
-    return render(request, 'register.html')
+def register(request: HttpRequest):
+    if request.GET.get('email') is None:
+        return render(request, 'register.html')
+
+    email = request.GET['email']
+    password = request.GET['password']
+    password_confirm = request.GET['password-confirm']
+
+    if password != password_confirm:
+        raise Http404()
+    
+    user = User(username=email, password=password, email=email)
+    user.save()
+
+    return HttpResponse('user registered successfuly')
+
+
+    
+    return redirect('/')
 
 
 @login_required
-def test(request):
-    pass
+def test_func(request):
+    return HttpResponse('hello')
