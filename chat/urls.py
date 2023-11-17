98e26db523ec80f562a0936277c9b47269b679f2
Erfanam <s.e.hashemy.1383@gmail.com>
2023-11-17 09:15:16 +0330

    added messages to database

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-    path('chat-list/<int:page>', views.chat_list, name='chat_list'),
+    path('chat-list', views.chat_list, name='chat_list'),
     path('create-chat', views.create_chat, name='create_chat'),
     path('chat-details', views.chat_details, name='chat_details'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse, Http404
 from django.contrib.auth import authenticate, login as create_token
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from .models import User, Bot, Conversation
+from .models import User, Bot, Conversation, Message
+from .forms import ChatForm
+from openai import OpenAI
 
 
 def login(request: HttpRequest):
@@ -19,7 +22,7 @@ def login(request: HttpRequest):
         raise Http404()
 
     create_token(request, user)
-    return redirect(reverse('chat_list', kwargs={'page': 0}))
+    return redirect(reverse("chat_list", kwargs={"page": 0}))
 
 
 def register(request: HttpRequest):
@@ -40,14 +43,18 @@ def register(request: HttpRequest):
 
 
 @login_required
-def chat_list(request: HttpRequest, page: int = 0):
+def chat_list(request: HttpRequest):
     PAGE_LENGTH = 10
 
-    context = {'1': 'SALAM', '2':'KHOOBI', '3': 'CHEKHABAR'}
+    context = {"1": "SALAM", "2": "KHOOBI", "3": "CHEKHABAR"}
     bot_list = Bot.objects.all()
-    conversations_list = Conversation.objects.all()[page * PAGE_LENGTH: page * PAGE_LENGTH + PAGE_LENGTH]
-    
-    return render(request, "chat-list.html", {"bot_list": bot_list, 'conversations_list': conversations_list})
+    conversations_list = Conversation.objects.all()
+
+    return render(
+        request,
+        "chat-list.html",
+        {"bot_list": bot_list, "conversations_list": conversations_list},
+    )
 
 
 @login_required
@@ -57,4 +64,44 @@ def create_chat(request: HttpRequest):
 
 @login_required
 def chat_details(request: HttpRequest):
-    return render(request, "chat-details.html")
+    if request.method != "POST":
+        form = ChatForm()
+        return render(request, "chat-details.html", {"form": form})
+
+    form = ChatForm(request.POST)
+    if not form.is_valid():
+        raise Http404("Please fill the form properly")
+
+    client = OpenAI(
+        api_key="C9vpBLBZkAbvbvimiOogyxJ8bOiLRkv3",
+        base_url="https://openai.torob.ir/v1",
+    )
+    chat_completion = json.loads(
+        client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": form.cleaned_data["query"],
+                },
+            ],
+            model="gpt-3.5-turbo",
+            frequency_penalty=0,
+            n=1,
+        )
+    )
+
+    
+
+    user_message = Message(message=form.cleaned_data["query"])
+    user_message.save()
+    bot_message = Message(message=chat_completion["choices"][0]["message"]["content"])
+    bot_message.save()
+
+    return render(
+        request,
+        "chat-details.html",
+        {
+            "form": form,
+            "responses": bot_message.message,
+        },
+    )
